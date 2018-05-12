443f8a9f04cc86785bcdd8344655510f7bf39201
Adam Spann <Bas-Man@users.noreply.github.com>
2018-05-12 21:56:09 +0900

    Added my personal code sample with additional comments to help if people wish to trigger a library scan after import.

---
+++ Examples/myPersonalCode.pl
@@ -0,0 +1,32 @@
+#!/bin/perl
+
+# This is used during development to confirm code works in my local environment.
+#use lib "/Users/aspann/dev/BAS-TVShow-Import/lib/";
+
+# Note I do not personally do any case testing as I have been using this code for some time and am very
+# familiar with its operation.
+
+use strict;
+use warnings;
+
+use Video::File::TVShow::Import;
+
+our $exceptionList = "S.W.A.T.2017:S.W.A.T 2017";
+
+my $obj = Video::File::TVShow::Import->new();
+
+$obj->newShowFolder("/Volumes/Drobo/completed");
+$obj->showFolder("/Volumes/Drobo/TV Shows");
+
+$obj->createShowHash();
+
+$obj->processNewShows();
+
+# if you wish to use the plex command here you will need to check what number matches your TV Library to trigger
+# a reload of the correct items.
+# See https://support.plex.tv/articles/201242707-plex-media-scanner-via-command-line/ for details.
+my $plexCommand = "/Applications/Plex\\ Media\\ Server.app/Contents\\/MacOS\\/Plex\\ Media\\ Scanner -s -c 1 > /dev/null 2>&1";
+
+system($plexCommand);
+
+exit 0;
