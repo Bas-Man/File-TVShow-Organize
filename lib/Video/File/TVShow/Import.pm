816038f8158c2a941606eaccf943025ad9681c29
Adam Spann <Bas-Man@users.noreply.github.com>
2018-05-13 23:07:25 +0900

    updated importShow corrected a type where japanese yen character had been entered in stead of back slash. Also displayed the error code from a failed rsync command.

--- lib/Video/File/TVShow/Import.pm
+++ lib/Video/File/TVShow/Import.pm
@@ -341,7 +341,8 @@ sub importShow {
     }
   } else {
     #report failed processing? Error on rsync command return code
-    print "Something went very wrong. Rsync failed for some reason.¥n"
+    print "## Something went very wrong. Rsync failed for some reason.\n"
+    print "## rsync err $?\n";
   }
   return $self;
 
